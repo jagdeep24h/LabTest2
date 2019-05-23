97d4c43498a6a4eedccabc8c7a50db7025bb4108
jagdeep24h <50706283+jagdeep24h@users.noreply.github.com>
2019-05-23 07:27:25 -0700

    Test case 3 by adding negative input for addition

--- Class1.cs
+++ Class1.cs
@@ -14,6 +14,26 @@ namespace CalcAppTest
     class CalcTests
     {
         [Test]
+        //Case 1
+        public void GetAddition_Input6point46and5point44_Returns12point00()
+        {
+
+            //Arrange
+            double number1 = 6.46;
+            double number2 = 5.44;
+
+            double expectedResult = number1 + number2;
+
+            Calc testCalc = new Calc(number1, number2);
+
+            //Act
+            double actualResult = testCalc.GetAddition();
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        // Case 2
         public void GetAddition_Input112and140_Returns252()
         {
 
@@ -31,6 +51,26 @@ namespace CalcAppTest
             //Assert
             Assert.AreEqual(expectedResult, actualResult);
         }
-        
+        [Test]
+        // Case 2
+        public void GetAddition_Inputminus55and55_Returnsminus40()
+        {
+
+            //Arrange
+            double number1 = -55;
+            double number2 = 15;
+
+            double expectedResult = number1 + number2;
+
+            Calc testCalc = new Calc(number1, number2);
+
+            //Act
+            double actualResult = testCalc.GetAddition();
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
     }
 }
