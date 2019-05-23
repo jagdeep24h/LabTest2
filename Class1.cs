e8fec4b1a78627ab496cfb17145557f0878cd05d
jagdeep24h <50706283+jagdeep24h@users.noreply.github.com>
2019-05-23 07:01:28 -0700

    First Test case for Addition operation created

---
+++ Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AwesomeCalculator;
+using NUnit.Framework;
+
+
+namespace CalcAppTest
+
+{
+    [TestFixture]
+    class CalcTests
+    {
+        [Test]
+        public void GetAddition_Input6point46and5point54_Returns12point0()
+        {
+
+            //Arrange
+            double number1 = 6.46;
+            double number2 = 5.54;
+
+            double expectedResult = number1 + number2;
+
+            Calc testCalc = new Calc(number1, number2);
+
+            //Act
+            double actualResult = testCalc.GetAddition();
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        
+    }
+}
